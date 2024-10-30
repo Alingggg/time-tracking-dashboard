b17bdfd5c8b133c79451342a6b01c7b01d095309
Alingggg <lapitangeof2@gmail.com>
2024-10-30 22:46:08 +0800

    create boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Button from './components/Button';
+
+function App() {
+  return (
+    <>
+      <h1>Hi</h1>
+      <Button></Button>
+    </>
+  );
+}
+
+export default App;
